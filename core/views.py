c8e044fc5cca86a64ec31310894b53468b1e4dcb
Vitor Angheben <vitor_angheben@hotmail.com>
2023-03-10 09:54:15 -0300

    Project working but the facebook social administration is not, try again later

--- core/views.py
+++ core/views.py
@@ -1,18 +1,11 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
-    """
-    Esse primeiro atributo da classe 'LoginRequiredMixin', não permite que o usuário acesse ao template sem estar logado
-    o deixando em um loop infinito até ele logar
-    """
     template_name = 'index.html'
 
 
 class LoginView(TemplateView):
-    """
-    Essa é a página de login
-    """
     template_name = 'login.html'
+
